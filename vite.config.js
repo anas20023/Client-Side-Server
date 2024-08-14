92d9cc5e940ff9c6cf49db483ae0e34fc3a372db
Anas Ibn Belal <yorder07@gmail.com>
2024-08-14 23:00:57 +0600

    chore: Update backend URL in proxy configuration

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // This will proxy requests starting with /api to your backend server
       "/api": {
-        target: "https://cloud-file-storage-backend.vercel.app/", // Your backend URL
+        target: "https://cloud-file-storage-backend.vercel.app", // Your backend URL
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""), // Optionally rewrite the path
